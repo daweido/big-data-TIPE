29c20bd2cc333631d49b3dff7d23f5d7397b2d81
David <david.rigaux@eisti.fr>
2016-12-08 14:09:12 +0100

    Rendu cahier des charges + Modif Kmeans
    Rendu des cahier des charges
    Plus derniere version de presentation kMeans
    et la premiere version de kmeans + celle en cours de modification mais erreur d’affectation a la ligne

--- CPI2/algorithmes/kmeans/kmeans/tipe.py
+++ CPI2/algorithmes/kmeans/kmeans/tipe.py
@@ -12,9 +12,12 @@ k centroïdes initialisé aléatoirement, et un autre graphique après avoir fai
 
 import matplotlib.pyplot as plt
 import numpy as np #Import numpy locally in variable np
+import sys
+import math as m
 import seaborn as sns; sns.set()
 import matplotlib.cm as cmx
 import matplotlib.colors as colors
+from numpy import linalg as LA
 
 #Fonction read_data permet de lire les données d'un fichier formaté et de resortir un array des éléments du fichier
 def read_data(file_name):
@@ -29,24 +32,66 @@ def read_data(file_name):
 		y = d[esp+1:]
 		dataread.append((int(x), int(y)))
 	del dat
-	return dataread
+	return np.asarray(dataread)
 
 #Fonction init_centroids ressort k centroids aléatoire
 def init_centroids (datas, k):
 	centroids_create = datas.copy()
 	np.random.shuffle(centroids_create)
-	return centroids_create[:k]
+	return np.asarray9centroids_create[:k])
+
+
 
 '''Fonction close_centroid nous permet de calculer pour chaque élément le centroides qui lui est le plus proche et le ressort sous forme
 d'array'''
 def close_centroid(datas, centds):
-	dists = np.sqrt(((datas - centds[:, np.newaxis])**2).sum(axis=2))
-	return np.argmin(dists, axis=0)
+	pproche = []
+	for ind in datas:
+		normes = []
+		for cen in centds:
+			normes.append(m.sqrt((cen[0]-ind[0])**2 + (cen[1]-ind[1])**2))
+		pproche.append(normes.index(min(normes)))
+	return np.asarray(pproche)
+
+
 
 '''Fonction movmnt_centroids calcul la moyenne des vecteurs de chaque points par rapport à son centroide le plus proche
 qui le déplace ensuite par rapport à ce vecteur moyen'''
 def movmnt_centroids(dat, pclose, cent):
-	return np.array([dat[pclose==k].mean(axis=0) for k in range(cent.shape[0])])
+	for i in range(len(cent)):
+		indices = np.where(pclose == i)
+		sumx = 0
+		sumy = 0
+		for j in indices:
+			sumx += dat[j][0]
+			sumy += dat[j][1]
+		card = len(indices)
+		cent[i][0] = sumx//card
+		cent[i][1] = sumy//card
+	return cent
+
+#Fonction qui affecte les nouvelles positions des centroids
+def ite_movmt_centro(data,centr):
+	done = False
+	epsi = 0.000000000000001
+	def ecart (copie,origin,index):
+		ex = abs(ccentre[index][0] - centr[index][0])
+		ey = abs(ccentre[index][1] - centr[index][1])
+		return True if (ex <= epsi and ey <= epsi) else False
+
+	while (not done):
+		ccentre = centr.copy
+		pp = close_centroid(data,centr)
+		centr = movmnt_centroids(data, pp, centr)
+		for i in range(len(ccentre)):
+			if ecart(ccentre,centr,i):
+				if (i == (len(ccentre)-1)):
+					done = False
+				else:
+					pass
+			else:
+				break
+	return centr
 
 #get_cmap permet de créer un array avec N couleurs différente
 def get_cmap(N):
@@ -56,6 +101,9 @@ def get_cmap(N):
 		return scalar_map.to_rgba(index)
 	return map_index_to_rgb_color
 
+
+
+
 '''Fonction kmeans, est la fonction principale qui appelle toute les fonctions ci-dessus. Elle sort un array avec les
 coordonnées de chaque centroide après que les centroides convergent et affiche dans une figure le grpahique de chaque
 chaque points avant et après l'application de kmeans'''
@@ -64,7 +112,8 @@ def kmeans(data, k):
 	centroids = init_centroids(data, k)
 	cmap = get_cmap(k+1)
 	ccentroids = close_centroid(data, centroids)
-	for i in range(len(ccentroids) - 1):
+
+	for i in range(len(ccentroids)):
 		plt.scatter(data[i, 0], data[i, 1], c=cmap(ccentroids[i]))
 	plt.scatter(centroids[:, 0], centroids[:, 1], s=20, c='black')
 	plt.title('Avant mouvement des centroïdes')
@@ -72,8 +121,7 @@ def kmeans(data, k):
 	plt.axis('off')
 
 	plt.subplot(122)
-	clo = close_centroid(data, centroids)
-	centroids = movmnt_centroids(data, clo, centroids)
+	centroids = ite_movmt_centro(data,centroids)
 	ccentroids = close_centroid(data, centroids)
 	for i in range(len(ccentroids) - 1):
 		plt.scatter(data[i, 0], data[i, 1], c=cmap(ccentroids[i]))
@@ -89,10 +137,17 @@ def kmeans(data, k):
 	plt.show()
 	return centroids
 
+
+
 #Fonction kmeans_rand appelle kmean avec des données aléatoire
 def kmeans_rand(min,max,n,k):
 	return kmeans(np.random.randint(min, max, size=(n, 2)), k)
 
+
+
 #Fonction kmeans_file appelle kmean avec des données pris d'un fichier
 def kmeans_file(file_n,k):
 	return kmeans(np.array(read_data(file_n)), k)
+
+
+#Chercher comment calculer les itérations
--- CPI2/spark/tests/test1.scala
+++
@@ -1,5 +0,0 @@
-val conf = new SparkConf().setAppName("WordCount")
-val sc = new SparkContext(conf)
-
-val input = sc.textFile(inputFile)
-val words = input.flatMap
